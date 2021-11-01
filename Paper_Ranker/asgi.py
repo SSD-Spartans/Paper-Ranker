2a925a22ef2a2e19f2a3dd4e8765d207524d2115
Kushal7788 <kushshah777888@gmail.com>
2021-11-01 19:23:10 +0530

    Set up of Django Project -Added gitignore file -Added readme and requirements file

---
+++ Paper_Ranker/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Paper_Ranker project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Paper_Ranker.settings')
+
+application = get_asgi_application()
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Paper Ranker
+
+----
+
+## Steps to Run the project
+
+- Create virtual env with following command
+> python3 -m venv venv
+- Activate the virtual env with following command
+> source venv/bin/activate
+- Install requirements with following command
+> pip3 install -r requirements.txt
+- Run the Django project with following command
+> python3 manage.py runserver
